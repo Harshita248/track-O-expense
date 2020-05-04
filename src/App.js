faa158b5e99899806ec56fc956458ce473888b9e
Harshita248 <harshitaraj248@gmail.com>
2020-05-04 15:10:13 +0530

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header } from './component/header';
+import { Balance } from './component/bal';
+import { IncomeExpenses } from './component/income';
+import { TransactionList } from './component/transac_list';
+import { AddTransaction } from './component/transac_add';
+
+import { GlobalProvider } from './content/global';
+
 import './App.css';
+//backgroundImage: url("./public/bg.jpg");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
